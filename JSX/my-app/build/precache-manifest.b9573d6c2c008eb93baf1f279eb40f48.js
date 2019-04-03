0c769d7b64eb154b0acabe08e33a58411d43a96c
Blaine Smith <ringospy@gmail.com>
2019-04-03 13:03:36 -0500

    editing the gitignore

---
+++ JSX/my-app/build/precache-manifest.b9573d6c2c008eb93baf1f279eb40f48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "628141f603a315178b4b",
+    "url": "./static/js/main.1f4d857a.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "./static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "628141f603a315178b4b",
+    "url": "./static/css/main.6f28e1ad.chunk.css"
+  },
+  {
+    "revision": "e272a16d08a9b309effa32f95be0621c",
+    "url": "./index.html"
+  }
+];
